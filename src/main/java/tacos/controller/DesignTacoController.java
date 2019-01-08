564fff79cbb7646ec8f1cf9a316114dd5a477e1c
Jorge <georgevazj@gmail.com>
2019-01-08 22:45:03 +0100

    Model validation

--- src/main/java/tacos/controller/DesignTacoController.java
+++ src/main/java/tacos/controller/DesignTacoController.java
@@ -4,9 +4,13 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -50,9 +54,13 @@ public class DesignTacoController {
 	}
 	
 	@PostMapping
-	public String processDesign(Taco taco) {
+	public String processDesign(@Valid Taco design, Errors errors) {
+		if (errors.hasErrors()) {
+			return "design";
+		}
+		
 		// TODO - Save the Taco design...
-		log.info("Processing taco: " + taco);
+		log.info("Processing taco: " + design);
 		return "redirect:/orders/current";
 	}
 
