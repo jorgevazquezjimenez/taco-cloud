9f8384f2420dc86c2c074e7a7e1c71d102b92784
Jorge <georgevazj@gmail.com>
2019-01-10 13:01:56 +0100

    JDBC implementation

---
+++ src/main/java/tacos/repository/IngredientRepository.java
@@ -0,0 +1,11 @@
+package tacos.repository;
+
+import tacos.domain.Ingredient;
+
+public interface IngredientRepository {
+	
+	Iterable<Ingredient> findAll();
+	Ingredient findOne(String id);
+	Ingredient save(Ingredient ingredient);
+
+}
