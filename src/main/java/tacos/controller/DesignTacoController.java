f30283b27ba13b05298326d257c09a7cfee151ab
Jorge Vazquez <georgevazj@gmail.com>
2019-01-11 10:06:51 +0100

    SQL scripts added

--- src/main/java/tacos/controller/DesignTacoController.java
+++ src/main/java/tacos/controller/DesignTacoController.java
@@ -1,11 +1,13 @@
 package tacos.controller;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -19,26 +21,26 @@ import tacos.domain.Ingredient;
 import tacos.domain.Ingredient.Type;
 import tacos.domain.Order;
 import tacos.domain.Taco;
+import tacos.repository.IngredientRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/design")
 public class DesignTacoController {
 	
+	private final IngredientRepository repository; 
+	
+	@Autowired
+	public DesignTacoController(IngredientRepository repository) {
+		super();
+		this.repository = repository;
+	}
+
 	@GetMapping
 	public String showDesignForm(Model model) {
-		List<Ingredient> ingredients = Arrays.asList(
-				new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-				new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-				new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-			    new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-			    new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-			    new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-			    new Ingredient("CHED", "Cheddar", Type.CHEESE),
-			    new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-			    new Ingredient("SLSA", "Salsa", Type.SAUCE),
-			    new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-				);
+		List<Ingredient> ingredients = new ArrayList<>();
+		repository.findAll().forEach(i -> ingredients.add(i));
+		
 		Type[] types = Ingredient.Type.values();
 		for (Type type:types) {
 			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
